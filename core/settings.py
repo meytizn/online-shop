29da43a7a20dc98d8639506281433735f244aec5
meytizn <zamanianmehdi23desktop@gmail.com>
2025-03-31 17:49:16 +0330

    postgres config added not used

--- core/settings.py
+++ core/settings.py
@@ -74,12 +74,32 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'your_database_name',
+#         'USER': 'your_username',
+#         'PASSWORD': 'your_password',
+#         'HOST': 'localhost',
+#         'PORT': '5432', # my port is 5433
+#     }
+# }
+
+
+
+
 
 
 # Password validation
